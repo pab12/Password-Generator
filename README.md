23054e1f3f64795837b28a9c33684dd5c73f9a68
Paul Barcenas <paulbarcenas@yahoo.com>
2021-08-29 15:04:07 -0500

    added screenshot, updated README.md file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Password-Generator
+
+## Purpose 
+To help users get password following what criteria they have in mind. As in the following, uppercase, lowercase, numeric, or special characters at given a specific length with in 8-128 characters.
+
+## Built with 
+* HTML
+* CSS
+* JS
+
+## Screen Shot
+![Password Generator website](assets/images/website-screenshoot.png)
+## Websites
+https://github.com/pab12/Password-Generator
+https://pab12.github.io/Password-Generator/
+
+## Contribution
+Made with ❤️ by Paul Barcenas
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-// Assignment code here
+//Criteria var here
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharacters = ["!", "@", "#","#","$","%","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?", "@","[","]","^","_","`","{","}","|","~"];
 
-
+//generate the password function
 function generatePassword(){
   var lowerCaseAnswer = confirm("Do you want your password to contain lowercase?");
 var upperCaseAnswer = confirm("Do you want your password to contain uppercase?");
@@ -46,6 +46,7 @@ var lengthOfPassword = parseInt(prompt("How many character slots do you want it 
 
   return password;
 }
+// how we get randomCharacter with a function for random
 function getRandomCharacter(array){
   var index = Math.floor(Math.random()*array.length);
   return array[index];
