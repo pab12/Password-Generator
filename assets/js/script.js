592840f886747f8b6caffb1ae143f41e40bf6ce8
Paul Barcenas <paulbarcenas@yahoo.com>
2021-08-28 23:22:19 -0500

    added generatePassword function with prompts and confirms for criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,43 @@ var numeric = ["0","1","2","3"];
 var specialCharacters = ["!", "@", "#"];
 
 
+function generatePassword(){
+  var lowerCaseAnswer = confirm("Do you want your password to contain lowercase?");
+var upperCaseAnswer = confirm("Do you want your password to contain uppercase?");
+var numericAnswer = confirm("Do you want your password to contain numeric?");
+var specialCharactersAnswer = confirm("Do you want your pasword to contain special characters?");
+var lengthOfPassword = parseInt(prompt("How many character slots do you want it to take. choose from 8-128."));
+  var password = "";
+  var characterTypes = [];
+  if(lowerCaseAnswer){
+    characterTypes = characterTypes.concat(lowerCase);
+  }
+  if(upperCaseAnswer) {
+    characterTypes = characterTypes.concat(upperCase);
+  }
+  if(numericAnswer) {
+    characterTypes = characterTypes.concat(numeric);
+  }
+  if(specialCharactersAnswer) {
+    characterTypes = characterTypes.concat(specialCharacters);
+  }
+  if(!lowerCaseAnswer && !upperCaseAnswer && !numericAnswer && !specialCharactersAnswer){
+    alert("Has to have at least one criteria");
+    return null;
+  }
+  if(typeof lengthOfPassword === 'number'){
+    if((lengthOfPassword<8) || (lengthOfPassword>128)){
+      alert("The number has to be between 8 and 128.");
+      return null;
+    }
+  } else {
+    alert("This has to be a number");
+    return null;
+  }
+ 
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
